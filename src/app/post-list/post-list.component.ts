826a1e8dea8eee7ecabb44ee06637f05006b2714
Gio-Padua <112618866+Gio-Padua@users.noreply.github.com>
2022-10-12 16:03:20 +0800

    Gio dev (#2)
    * delete p
    
    * new nav bar
    
    * new header
    
    * changes new

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../post.model';
+import { PostService } from '../post.service';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-post-list',
@@ -7,20 +9,13 @@ import { Post } from '../post.model';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  listOfPost: Post[]=[
-    new Post(
-      'Best Beaches in the World',
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beach-quotes-1559667853.jpg',
-      'Lounge on the best Philippines beaches! If youre looking for pristine beaches with crystal clear waters and postcard-worthy views, the Philippines offers you a lot of choices! But which ones should you visit? Read on to learn about the countrys top beaches.',
-      'Larena',
-      new Date()
+  
+  listOfPost: Post[]=[]
 
-    )
-  ]
-
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.listOfPost= this.postService.getPost();
   }
 
 }
